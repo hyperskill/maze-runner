5f082c7cf1e68f656d6acb5fde69ec0dac4cb599
nikolaiser <nickolayser@gmail.com>
2018-10-31 20:38:35 +0100

    another array type

--- src/mazeRunner/Maze.java
+++ src/mazeRunner/Maze.java
@@ -3,36 +3,44 @@ package mazeRunner;
 import javafx.util.Pair;
 
 public class Maze {
-    private String[][] maze;
+    private int[][] maze;
     private Pair<Integer,Integer> size = new Pair<>(20,20);
     public Maze(){
 
-        maze = new String[size.getKey()][size.getValue()];
+        maze = new int[size.getKey()][size.getValue()];
         for(int i = 0; i < size.getKey(); i ++)
             for(int j = 0;j < size.getValue(); j++)
                 if(i == j)
-                    maze[i][j] = Field.Pass.getVal();
+                    maze[i][j] = Field.Pass.getKey();
                 else
-                    maze[i][j] = Field.Wall.getVal();
+                    maze[i][j] = Field.Wall.getKey();
 
     }
 
     public void printMaze(){
-        for(String[] string:maze) {
-            for (String symb : string)
-                System.out.print(symb);
+        for(int[] string:maze) {
+            for (int symb : string)
+                System.out.print(symb == Field.Pass.getKey() ? Field.Pass.getVal():Field.Wall.getVal());
             System.out.println();
         }
 
     }
     enum Field{
-        Pass("  "),Wall("\u2592");
+        Pass("  ",0),Wall("\u2592",1);
         private String val;
-        Field(String val){
+        private int key;
+        Field(String val,int key){
             this.val = val;
+            this.key = key;
         }
         String getVal(){
             return val;
         }
+
+        public int getKey() {
+            return key;
+        }
+
+
     }
 }
