852f71110b76ec9f6d044ebbdb04ca8b84c67771
DianaBogdanova98 <diankabogdanova@mail.ru>
2018-11-08 22:18:43 +0300

    print maze

--- src/mazeRunner/Main.java
+++ src/mazeRunner/Main.java
@@ -2,6 +2,24 @@ package mazeRunner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+        int[][] maze = new int [20][20];
+        for (int i = 0;i<20; i++){
+            for (int j = 0; j<20; j++){
+                if ( j == 0||j==19||i==0||i==19)
+                    maze[i][j] =1;
+            }
+        }
+        for (int i = 0;i<20; i++){
+            for (int j = 0; j<20; j++){
+                if (  maze[i][j] == 0) {
+                    System.out.print("  ");
+                } else if ( maze[i][j] == 1){
+                    System.out.print("\u2592");
+                }
+            }
+            System.out.print("\n");
+        }
+
     }
 }
