8ef0f03e25714da5a06a942432765619ff5efd79
max <tolstyu@gmail.com>
2018-11-12 10:29:55 +0300

    Stage 1

--- src/mazeRunner/Main.java
+++ src/mazeRunner/Main.java
@@ -1,7 +1,32 @@
 package mazeRunner;
 
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+        int[][] array = new int[20][20];
+        for(int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                array[i][j] = random.nextInt(2);
+            }
+        }
+        for(int i = 0; i < 20; i++)
+        {
+            for(int j = 0; j < 20; j++)
+            {
+                if(array[i][j] == 0)
+                {
+                    System.out.print("  ");
+                }else
+                {
+                    System.out.print("\u2592\u2592");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
