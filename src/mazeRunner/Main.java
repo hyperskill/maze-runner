a763c7944ef17b9f4cc156bc62c2876b1fc30754
algizame <31997677+algizame@users.noreply.github.com>
2018-11-13 18:48:45 +0300

    display the maze
    The code which draws the maze.

--- src/mazeRunner/Main.java
+++ src/mazeRunner/Main.java
@@ -2,6 +2,50 @@ package mazeRunner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        displayMaze(20, 20);
     }
+
+    public static void displayMaze(int columnsNumber, int linesNumber) {
+
+        int[][] maze = new int[columnsNumber][linesNumber];
+        int a = columnsNumber;
+        int b = linesNumber;
+
+        char isWall = '\u2593'; // 1 = isWall
+        char noWall = ' ';         // 0 = noWal
+
+//create Maze
+        for (int x = 0; x < columnsNumber; x++) {
+            for (int y = 0; y < linesNumber; y++) {
+
+                if (y == 1) {
+                    maze[x][y] = 0;
+                } //if y
+
+                else {
+                    maze[x][y] = 1;
+                }//else
+
+            } //for y
+        } //for x
+
+//display Maze
+        for (int x = 0; x < columnsNumber; x++) {
+            for (int y = 0; y < linesNumber; y++) {
+
+                if (y == 1) {
+                    System.out.print(noWall);
+                    System.out.print(noWall);
+                } //if y
+
+                else {
+                    System.out.print(isWall);
+                    System.out.print(isWall);
+                }//else
+
+            } //for y
+            System.out.println();
+        } //for x
+
+    } //display
 }
